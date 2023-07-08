d268bb278e5507f9ae56ab2f0efc491b84914ca7
Ky-Miller <kychr.miller@gmail.com>
2023-07-07 20:57:12 -0700

    updated to match assignment parameters

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PersonCard = (props) => {
     const {firstName, lastName, age, color} = props;
+    const [state, setState] = useState({
+        clickCount: age
+    });
+    const handleClick = () => {
+        setState({
+            clickCount: state.clickCount + 1
+        });
+    }
     return(
         <div>
             <h2>{lastName}, {firstName}</h2>
-            <p> Age: {age}</p>
+            <p> Age: {state.clickCount}</p>
             <p> Hair Color: {color} </p>
+            <button onClick={handleClick}>Birthday Button for {firstName} {lastName}</button>
         </div>
     );
 }
