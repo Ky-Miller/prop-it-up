3546cdab5e257e105753b9a9a10534a87f65cd9e
Ky-Miller <kychr.miller@gmail.com>
2023-07-07 21:01:25 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     
     <>
-      <PersonCard firstName={"Jane"} lastName={"Doe"} age = {"45"} color = {'Black'}/>
-      <PersonCard firstName={"John"} lastName={"Smith"} age = {"88"} color = {'Brown'}/>
-      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age = {"50"} color = {'Brown'}/>
-      <PersonCard firstName={"Maria"} lastName={"Smith"} age = {"62"} color = {'Brown'}/>
+      <PersonCard firstName={"Jane"} lastName={"Doe"} age = {45} color = {'Black'}/>
+      <PersonCard firstName={"John"} lastName={"Smith"} age = {88} color = {'Brown'}/>
+      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age = {50} color = {'Brown'}/>
+      <PersonCard firstName={"Maria"} lastName={"Smith"} age = {62} color = {'Brown'}/>
     </>
   );
 }
